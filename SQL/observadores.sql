31e0b5d6192e461d81e0e8bfb9e181c531958cbc
Santiago <santiagobanchero@gmail.com>
2023-09-24 23:44:58 -0300

    ejemplos de esquema y datos

---
+++ SQL/observadores.sql
@@ -0,0 +1,5 @@
+INSERT INTO Observadores VALUES(1,'Juan',   'Martin');
+INSERT INTO Observadores VALUES(2,'Diego',  'Ezquivel');
+INSERT INTO Observadores VALUES(3,'Rafael', 'Rodriguez');
+INSERT INTO Observadores VALUES(4,'Hugo',   'Conti');
+INSERT INTO Observadores VALUES(5,'José',   'Ortiz');
